a666e13e391d78c069c1dd9e43c688a2e44100f5
douglasmonteles <silvadouglas328@gmail.com>
2023-03-06 21:15:25 -0300

    Showing a screen with spring and javafx

---
+++ src/main/java/com/doug/jfx/store/JavaFxApplication.java
@@ -0,0 +1,12 @@
+package com.doug.jfx.store;
+
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class JavaFxApplication extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.show();
+    }
+}
--- src/main/java/com/doug/jfx/store/StoreApplication.java
+++ src/main/java/com/doug/jfx/store/StoreApplication.java
@@ -1,5 +1,6 @@
 package com.doug.jfx.store;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StoreApplication.class, args);
+		Application.launch(JavaFxApplication.class, args);
 	}
 
 }
