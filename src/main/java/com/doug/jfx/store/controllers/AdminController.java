e3e473dd370caef7c90bead9ce64dba978655c56
douglasmonteles <silvadouglas328@gmail.com>
2023-03-09 19:35:35 -0300

    Adding initial message

--- src/main/java/com/doug/jfx/store/controllers/AdminController.java
+++ src/main/java/com/doug/jfx/store/controllers/AdminController.java
@@ -2,13 +2,16 @@ package com.doug.jfx.store.controllers;
 
 import com.doug.jfx.store.builders.TableBuilder;
 import com.doug.jfx.store.builders.impl.TableBuilderImpl;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import org.springframework.stereotype.Component;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -32,7 +35,7 @@ public class AdminController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        borderPane.setCenter(new Label("Bem vindo!"));
     }
 
 }
