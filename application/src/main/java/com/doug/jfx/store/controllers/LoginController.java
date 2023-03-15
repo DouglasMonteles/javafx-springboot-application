1ed991898dd6d41ae742660a23d510dd484d7620
douglasmonteles <silvadouglas328@gmail.com>
2023-03-14 21:43:07 -0300

    Refactoring the validator method in login controller

--- application/src/main/java/com/doug/jfx/store/controllers/LoginController.java
+++ application/src/main/java/com/doug/jfx/store/controllers/LoginController.java
@@ -1,26 +1,19 @@
 package com.doug.jfx.store.controllers;
 
-import com.doug.jfx.store.builders.ScreenBuilder;
-import com.doug.jfx.store.builders.impl.ScreenBuilderImpl;
 import com.doug.jfx.store.enums.Routes;
+import com.doug.jfx.store.helpers.Validators;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXPasswordField;
 import io.github.palexdev.materialfx.controls.MFXTextField;
-import javafx.application.Application;
-import javafx.beans.binding.Binding;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.input.InputMethodEvent;
 import javafx.scene.input.MouseEvent;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 @Component
@@ -55,14 +48,7 @@ public class LoginController implements Initializable {
 
     private void validateLogin() {
         this.loginButton.setDisable(true);
-
-        this.username.textProperty().addListener((observable, oldValue, newValue) -> {
-            this.loginButton.setDisable(username.getText().isEmpty() || password.getText().isEmpty());
-        });
-
-        this.password.textProperty().addListener((observable, oldValue, newValue) -> {
-            this.loginButton.setDisable(username.getText().isEmpty() || password.getText().isEmpty());
-        });
+        Validators.validateFormSubmitButton(List.of(username, password), loginButton);
     }
 
 }
