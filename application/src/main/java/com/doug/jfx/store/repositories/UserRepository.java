851454a9ab8cb470b51366a230eb24769a5216dc
douglasmonteles <silvadouglas328@gmail.com>
2023-03-19 21:20:21 -0300

    Showing all users with isActive = true

--- application/src/main/java/com/doug/jfx/store/repositories/UserRepository.java
+++ application/src/main/java/com/doug/jfx/store/repositories/UserRepository.java
@@ -1,9 +1,18 @@
 package com.doug.jfx.store.repositories;
 
 import com.doug.jfx.store.models.User;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT user FROM User user WHERE user.isActive = TRUE")
+    @NotNull
+    List<User> findAll();
+
 }
