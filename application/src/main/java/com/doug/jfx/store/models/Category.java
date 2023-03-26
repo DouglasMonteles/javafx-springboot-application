71a754bf03aebf18721657cf449bb1472e0a813a
douglasmonteles <silvadouglas328@gmail.com>
2023-03-25 21:29:32 -0300

    Adding category model class

--- application/src/main/java/com/doug/jfx/store/models/Category.java
+++ application/src/main/java/com/doug/jfx/store/models/Category.java
@@ -1,2 +1,34 @@
-package com.doug.jfx.store.models;public class Category {
+package com.doug.jfx.store.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = {"id"})
+@Entity
+@Table(name = "tb_category")
+public class Category implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 444969447792771740L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "categories")
+    private List<Product> products = new ArrayList<>();
+
 }
