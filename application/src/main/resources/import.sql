c69d4a6999aeb86181ed3ae132e6348a235ac3d2
douglasmonteles <silvadouglas328@gmail.com>
2023-03-25 21:30:33 -0300

    Adding data to the database seeding

--- application/src/main/resources/import.sql
+++ application/src/main/resources/import.sql
@@ -14,3 +14,16 @@ INSERT INTO tb_user_roles (user_id, role_id) VALUES (1, 1);
 INSERT INTO tb_user_roles (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_roles (user_id, role_id) VALUES (2, 3);
 INSERT INTO tb_user_roles (user_id, role_id) VALUES (2, 4);
+
+INSERT INTO tb_category (name) VALUES ('Eletrônico');
+INSERT INTO tb_category (name) VALUES ('Cama, mesa e banho');
+INSERT INTO tb_category (name) VALUES ('Alimento perecível');
+INSERT INTO tb_category (name) VALUES ('Alimento não perecível');
+INSERT INTO tb_category (name) VALUES ('Esporte');
+
+INSERT INTO tb_product (name, price, description, measurement_type, measurement, is_available) VALUES ('Arroz', 10.99, 'Arroz bom de gosto', 1, 12, true);
+INSERT INTO tb_product (name, price, description, measurement_type, measurement, is_available) VALUES ('Toalha', 20.99, 'Toalha do bem 10 (antes do reboot)', 2, 2, true);
+
+INSERT INTO tb_product_category (product_id, category_id) VALUES (1, 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (2, 2);
+
